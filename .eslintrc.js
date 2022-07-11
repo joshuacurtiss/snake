761e3de9cb8eaa57714c5400908a03dd92769ed8
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2022-07-10 22:59:48 -0500

    eslint adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,10 @@ module.exports = {
     ],
     rules: {
         indent: ['error', 4],
+        camelcase: 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
     },
 };
